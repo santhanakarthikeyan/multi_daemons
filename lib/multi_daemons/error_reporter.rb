0b14e21bf9740ee0f23178d6b864739de5482e0b
santhanakarthikeyan <santhanakarthikeyan.s@gmail.com>
2018-12-12 12:44:09 +0530

    Added error_reporter to publish errors to Errbit

---
+++ lib/multi_daemons/error_reporter.rb
@@ -0,0 +1,16 @@
+module MultiDaemons
+  class ErrorReporter
+    attr_accessor :reporters
+
+    def self.report(exception, context_hash = {})
+      MultiDaemons.error_reporters.compact.each do |reporter|
+        begin
+          reporter.call(exception, self, context_hash)
+        rescue => inner_exception
+          puts inner_exception
+          puts inner_exception.backtrace.join("\n") unless inner_exception.backtrace
+        end
+      end
+    end
+  end
+end
